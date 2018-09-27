a732bd0688ba1fa8c2726095396cf5df0804c938
Edbergarn <te16.emil.edberg@umea.nti.se>
2018-09-27 11:13:48 +0200

    slut på lektionen

--- login.php
+++ login.php
@@ -10,17 +10,43 @@ session_start();
 </head>
 <body>
 <?php
-
+include 'db.php';
 $servername = "localhost";
 $dbname = "te16";
-if(isset($_POST['submit'])) // if($_POST['submit'] == "Logga in")
+if(isset($_SESSION['id']))
 {
-	include 'db.php';
+	echo "Du är inloggad";
+	echo "<h1>Welcome to my red room " . $_SESSION['username'] . ".</h1>";
+	echo '	
+		<form action="" method="POST">
+			<fieldset>
+				<legend>Ta bort min användare tack</legend>
+				<p>
+					<label for="password">Lösenord:</label>
+					<input type="password" name="password" id="password">
+				</p>
+				<p>
+					<input type="submit" name="delete" id="delete" value="Ta bort">
+				</p>
+			</fieldset>
+		</form>';
+	if (isset($_POST['delete'])) {
+		$statement = $dbh->prepare("DELETE FROM login WHERE id = :id");
+		$statement->bindParam(':id', $_SESSION['id']);
+		$statement->execute();
+		
+		
+	}
+//	DELETE FROM login WHERE id = $_SESSION['id'];
+
+}
+elseif(isset($_POST['submit'])) // if($_POST['submit'] == "Logga in")
+{
 	if(isset($_POST['username']) && isset($_POST['password']))
 	{
 		$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 		$password = $_POST['password'];
-		$statement = $dbh->prepare("SELECT * FROM login WHERE username = :username");
+		$statement = $dbh->prepare("SELECT * FROM login WHERE login . username = :username");
 		$statement->bindParam(':username', $username);
 		$statement->execute();
 		$row = $statement->fetch(PDO::FETCH_ASSOC);
@@ -28,9 +54,11 @@ if(isset($_POST['submit'])) // if($_POST['submit'] == "Logga in")
 		if($username == $row['username']
 			&& password_verify($password, $row['password']))
 		{
-			echo "<h1>Welcome to my red room.</h1>";
+			$_SESSION['id'] = $row['id'];
+			$_SESSION['username'] = $row['username'];
+			echo "<h1>Welcome to my red room " . $_SESSION['username'] . ".</h1>";
 			echo '	
-	<form action="index.php" method="POST">
+	<form action="" method="POST">
 		<fieldset>
 			<legend>Ta bort min användare tack</legend>
 			<p>
@@ -42,7 +70,7 @@ if(isset($_POST['submit'])) // if($_POST['submit'] == "Logga in")
 			</p>
 		</fieldset>
 	</form>';
-		}
+	}	
 		else
 		{
 			echo "<h1>Du har gjort väldigt fel!</h1>";
